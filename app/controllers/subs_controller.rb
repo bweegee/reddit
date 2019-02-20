4ea3792a28ff9dd96f6e063028fb1792e47db9c5
bweegee <bw33g33@gmail.com>
2019-02-20 11:42:24 -0700

    view pages addeda

--- app/controllers/subs_controller.rb
+++ app/controllers/subs_controller.rb
@@ -1,13 +1,54 @@
 class SubsController < ApplicationController
+  before_action :set_sub, only: [:show, :edit, :update, :destroy]
+
   def index
+    @subs = Sub.all
+    # render :index
   end
 
   def show
   end
 
   def new
+    @sub = Sub.new
+    render partial: "form"
+  end
+
+  def create
+    @sub = Sub.new(sub_params)
+
+    if @sub.save
+      redirect_to subs_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render partial: "form"
   end
+
+  def update
+    if @sub.update(sub_params)
+      redirect_to sub_path(@sub)
+      # shortcut: redirect_to @sub
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @sub.destroy
+    redirect_to subs_path
+  end
+
+  private
+    def sub_params
+      params.require(:sub).permit(:name)
+    end
+
+    def set_sub
+    #localhost:3000/subs/id
+    @sub = Sub.find(params[:id])
+    end
 end
